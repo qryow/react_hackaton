5b9cdbc5572ecd8fa2c2df79b1b27bfc2687d208
ada1533 <adakhanbelekov@gmail.com>
2023-09-03 18:23:35 +0600

    navbar and basic hamePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import MainRouter from './routing/MainRouter'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <MainRouter />
     </>
   )
--- src/routing/MainRouter.jsx
+++ src/routing/MainRouter.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import HomePage from '../components/HomePage'
+import HomePage from '../pages/HomePage'
 import RegisterPage from '../pages/AccountPages/RegisterPage'
+import SearchPage from '../pages/SearchPage'
+import LibraryPage from '../pages/LibraryPage'
+import CreatePage from '../pages/CreatePage'
+import LikedPage from '../pages/LikedPage'
+import LoginPage from '../pages/AccountPages/LoginPage'
 
 const MainRouter = () => {
   const ROUTES = [
@@ -14,7 +19,32 @@ const MainRouter = () => {
       id: 2,
       path: '/register',
       element: <RegisterPage />
-    }
+    },
+    {
+      id: 3,
+      path: '/login',
+      element: <LoginPage />
+    },
+    {
+      id: 4,
+      path: '/search',
+      element: <SearchPage />
+    },
+    {
+      id: 5,
+      path: '/library',
+      element: <LibraryPage />
+    },
+    {
+      id: 6,
+      path: '/create',
+      element: <CreatePage />
+    },
+    {
+      id: 7,
+      path: '/liked',
+      element: <LikedPage />
+    },
   ]
 
   return (
